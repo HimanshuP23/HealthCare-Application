db20d9d9677a666809575091cc7196b4cdd0e689
vedant_027 <vedant.thorat27@gmail.com>
2025-01-28 12:25:21 +0530

    jwt-role based done

--- backend/src/main/java/com/healthcare/BackendApplication.java
+++ backend/src/main/java/com/healthcare/BackendApplication.java
@@ -5,11 +5,13 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.healthcare.entities")
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/healthcare/security/JwtUtils.java
+++ backend/src/main/java/com/healthcare/security/JwtUtils.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Component;
 
+import com.healthcare.entities.Role;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -24,105 +26,55 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JwtUtils {
 
-	@Value("${SECRET_KEY}") //example of value injected as dependency , using SpEL
-	private String jwtSecret;
+    @Value("${SECRET_KEY}")
+    private String jwtSecret;
 
-	@Value("${EXP_TIMEOUT}")
-	private int jwtExpirationMs;
-	
-	
-	private Key key;
+    @Value("${EXP_TIMEOUT}")
+    private int jwtExpirationMs;
 
-	@PostConstruct
-	public void init() {
-		key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-	}
+    private Key key;
 
-	// will be invoked by Authentication controller , upon successful
-	// authentication
-	public String generateJwtToken(Authentication authentication) {
-		log.info("generate jwt token " + authentication);//contains verified user details
-		CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
-//JWT : userName,issued at ,exp date,digital signature(does not typically contain password , can contain authorities
-		return Jwts.builder() // JWTs : a Factory class , used to create JWT tokens
-				.setSubject((userPrincipal.getUsername())) // setting subject part of the token(typically user
-															// name/email)
-				.setIssuedAt(new Date())// Sets the JWT Claims iat (issued at) value of current date
-				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))// Sets the JWT Claims exp
-																					// (expiration) value.
-				// setting a custom claim , to add granted authorities
-				.claim("authorities", getAuthoritiesInString(userPrincipal.getAuthorities()))
-				// setting a custom claim , to add user id (remove it if not required in the project)
-				.claim("user_id",userPrincipal.getUser().getUserId())
-		
-				.signWith(key, SignatureAlgorithm.HS512) // Signs the constructed JWT using the specified
-															// algorithm with the specified key, producing a
-															// JWS(Json web signature=signed JWT)
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
 
-				// Using token signing algo : HMAC using SHA-512
-				.compact();// Actually builds the JWT and serializes it to a compact, URL-safe string
-	}
+    public String generateJwtToken(Authentication authentication) {
+        CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
 
-	// this method will be invoked by our custom JWT filter
-	public String getUserNameFromJwtToken(Claims claims) {
-		return claims.getSubject();
-	}
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .claim("role", userPrincipal.getUser().getRole().toString()) // Add role to claims
+                .claim("user_id", userPrincipal.getUser().getUserId())
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 
-	// this method will be invoked by our custom JWT filter
-	public Claims validateJwtToken(String jwtToken) {
-		// try {
-		Claims claims = Jwts.parserBuilder() //create JWT parser
-				.setSigningKey(key) //sets the SAME secret key for JWT signature verification
-				.build()//rets the JWT parser set with the Key
-				.parseClaimsJws(jwtToken) //rets JWT with Claims added in the body
-				.getBody();//=> JWT valid ,  rets the Claims(payload)
-		/*
-		 * parseClaimsJws - 
-		 * throws:UnsupportedJwtException -if the JWT body | payload does not represent any Claims 
-		 * JWSMalformedJwtException - if the JWT body | payload is not a valid 
-		 * JWSSignatureException - if the JWT signature validation fails
-		 * ExpiredJwtException - if the specified JWT is expired 
-		 * IllegalArgumentException - if the JWT claims body | payload is null or empty or only whitespace
-		 */
-		return claims;		
-	}
-	// Accepts Collection<GrantedAuthority> n rets comma separated list of it's
-	// string form
+    public String getUserNameFromJwtToken(Claims claims) {
+        return claims.getSubject();
+    }
 
-	private String getAuthoritiesInString(Collection<? extends GrantedAuthority> authorities) {
-		String authorityString = authorities.stream().
-				map(authority -> authority.getAuthority())
-				.collect(Collectors.joining(","));
-		System.out.println(authorityString);
-		return authorityString;
-	}
-	// this method will be invoked by our custom JWT filter to get list of granted authorities n store it in auth token
-		public List<GrantedAuthority> getAuthoritiesFromClaims(Claims claims) {
-		String authString = (String) claims.get("authorities");
-		List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(authString);
-		authorities.forEach(System.out::println);
-		return authorities;
-	}
-	// this method will be invoked by our custom JWT filter to get user id n store it in auth token
-			public Long getUserIdFromJwtToken(Claims claims) {
-				return Long.valueOf((int)claims.get("user_id"));			
-			}
-			
-			public Authentication populateAuthenticationTokenFromJWT(String jwt) {
-				// validate JWT n retrieve JWT body (claims)
-				Claims payloadClaims = validateJwtToken(jwt);
-				// get user name from the claims
-				String email = getUserNameFromJwtToken(payloadClaims);
-				// get granted authorities as a custom claim
-				List<GrantedAuthority> authorities = getAuthoritiesFromClaims(payloadClaims);
-				// get userId as the custom claim		
-				Long userId=getUserIdFromJwtToken(payloadClaims);
-				// add user name/email , user id n  granted authorities in Authentication object
-				UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(email,userId,
-						authorities);
-				System.out.println("is authenticated "+token.isAuthenticated());//true
-				return token;
-		
-			}
+    public Claims validateJwtToken(String jwtToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
 
+    public Role getRoleFromJwtToken(Claims claims) {
+        return Role.valueOf((String) claims.get("role")); // Fetch role from claims
+    }
+
+    public Authentication populateAuthenticationTokenFromJWT(String jwt) {
+        Claims payloadClaims = validateJwtToken(jwt);
+        String email = getUserNameFromJwtToken(payloadClaims);
+        Role role = getRoleFromJwtToken(payloadClaims);
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_" + role.toString());
+
+        Long userId = Long.valueOf((int) payloadClaims.get("user_id"));
+        return new UsernamePasswordAuthenticationToken(email, userId, authorities);
+    }
 }
